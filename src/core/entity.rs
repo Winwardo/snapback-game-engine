661f77b512daf5c92b1979a059183614eb764390
Topher Winward <hi@topher.io>
2015-08-25 18:21:12 +0100

    Entity generation

--- src/core/entity.rs
+++ src/core/entity.rs
@@ -1,11 +1,30 @@
-extern crate time;
+use std::rc::Rc;
 
 pub struct Entity {
-	id: u64,
+	id: u32,
 }
 
-impl Entity {
-	pub fn new() -> Entity {
-		Entity { id: time::precise_time_ns() }
+/// Master of all existing entities.
+struct Entities {
+	entities: Vec<Rc<Entity>>,
+	active: Vec<bool>
+}
+
+impl Entities {
+	pub fn new(self) -> Rc<Entity> {
+		let new_id = self.first_inactive_space();
+
+		Rc::new(
+			Entity { id: new_id, })
+	}
+
+	fn first_inactive_space(self) -> u32 {
+		for i in 0..self.active.len() {
+			if self.active[i] == false {
+				return i as u32;
+			}
+		}
+
+		return 0;
 	}
 }
